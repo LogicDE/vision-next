119510f82ac61ccf3dd5ef547a6727d407cc024e
jemiliogzz <jorgeemilio.gonzalez@udem.edu>
2025-09-03 19:26:14 -0600

    login-app
    front..., creo

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cta: "#BFC5B9",
+        "cta-dark": "#AEB2A6", // color para hover
+      },
+    },
+  },
+  plugins: [],
+};
